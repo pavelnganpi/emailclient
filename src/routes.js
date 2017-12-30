e0dde870bd37d78d314fe83d51fa7311ddcf06a6
pavey nganpi <paveynganpi@paveys-MacBook-Pro.local>
2017-12-30 00:06:35 -0500

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+const routes = Router();
+
+/**
+ * GET home page
+ */
+routes.get('/', (req, res) => {
+  res.status(200).json({message: " welcome email client apis"})
+});
+
+routes.get('/list', (req, res, next) => {
+  const { title } = req.query;
+
+  if (title == null || title === '') {
+    // You probably want to set the response HTTP status to 400 Bad Request
+    // or 422 Unprocessable Entity instead of the default 500 of
+    // the global error handler (e.g check out https://github.com/kbariotis/throw.js).
+    // This is just for demo purposes.
+    next(new Error('The "title" parameter is required'));
+    return;
+  }
+
+  res.render('index', { title });
+});
+
+export default routes;
